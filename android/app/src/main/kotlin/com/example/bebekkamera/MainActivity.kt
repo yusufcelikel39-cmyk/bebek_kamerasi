e577ee1bdbadb7fa546eeb4e4db4e792fbcd6bfb
yusufcelikel39-cmyk <yusufcelikel39@gmail.com>
2026-09-10 01:43:21 +0300

    ilk commit

---
+++ android/app/src/main/kotlin/com/example/bebekkamera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bebekkamera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ bebek_kamerasi/android/app/src/main/kotlin/com/example/bebek_kamerasi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bebek_kamerasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
